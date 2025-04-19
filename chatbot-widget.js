53b2512ed71e977338cc0f3bacdd3c46c225dc78
Mustafa.Hassan <mustafa.hassan@azets.com>
2025-04-19 22:12:52 +0200

    Title Font error

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -406,7 +406,7 @@ window.initChatbot = function initChatbot(config = {}) {
     // Style the text inside the infoContainer
     infoMessage.style.fontWeight = 'bold';
     infoMessage.style.marginTop = '10px';
-    infoMessage.style.color = '#333';
+    infoMessage.style.color = '#000';
 
     // Set height, max-height, and overflow for chat messages to ensure scrollability
     //chatMessages.style.minHeight = '200px';
